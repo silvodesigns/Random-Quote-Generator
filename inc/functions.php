21f78e0a76dfd1d50cf73bb723473a00d56f22fe
Kevin Silvestre <kevsilvo@gmail.com>
2020-05-09 05:20:56 -0400

    created array holding quotes

--- inc/functions.php
+++ inc/functions.php
@@ -1,7 +1,17 @@
+<?php
+
 // PHP - Random Quote Generator
 
 // Create the Multidimensional array of quote elements and name it quotes
 // Each inner array element should be an associative array
+$quote = array (
+    array('Junko Kaname' => 'Just because you keep trying to to the righ thing, doesn\'t mean
+    you are garanteed a happy ending.'),
+    array("Pain", "To understand pain, you must know pain"),
+    array("Goku", "Power comes in a response to a need, not a desire. You have to create that need"),
+    array("L Lawliet", "By trying too hard, we put ourselves at a greater risk"),
+    array("Armin Arlet", "A person who cannot give up anything , can change nothing")
+);
 
 
 
@@ -10,3 +20,6 @@
 
 
 // Create the printQuote funtion and name it printQuote
+
+
+?>
