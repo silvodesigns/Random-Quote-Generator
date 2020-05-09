d4513733810343ff2e8b9604ccfcc378bb242166
Kevin Silvestre <kevsilvo@gmail.com>
2020-05-09 05:34:23 -0400

    edited array with right values

--- inc/functions.php
+++ inc/functions.php
@@ -5,12 +5,32 @@
 // Create the Multidimensional array of quote elements and name it quotes
 // Each inner array element should be an associative array
 $quote = array (
-    array('Junko Kaname' => 'Just because you keep trying to to the righ thing, doesn\'t mean
-    you are garanteed a happy ending.'),
-    array("Pain", "To understand pain, you must know pain"),
-    array("Goku", "Power comes in a response to a need, not a desire. You have to create that need"),
-    array("L Lawliet", "By trying too hard, we put ourselves at a greater risk"),
-    array("Armin Arlet", "A person who cannot give up anything , can change nothing")
+    array(
+        'source'=> 'Junko Kaname',
+        'quote' => 'Just because you keep trying to to the righ thing, doesn\'t mean
+        you are garanteed a happy ending.',
+        'citation' => 'Puella Magi'
+   ),
+    array(
+        'source' => 'Pain',
+        'quote' => 'To understand pain, you must know pain',
+        'citation' => 'Naturo Shippuden'
+    ),
+    array(
+        'Source' => 'Goku',
+        'Quote' => 'Power comes in a response to a need, not a desire. You have to create that need',
+        'citation' => 'Dragon Ball Z'
+    ),
+    array(
+        'Source' => 'L Lawliet',
+        'Quote' => 'By trying too hard, we put ourselves at a greater risk',
+        'citation' => 'Death Note'
+    ),
+    array(
+        'Source' => 'Armin Arlet',
+        'Quote' => 'A person who cannot give up anything , can change nothing',
+        'citation' => 'Attack on Titans'
+    ),
 );
 
 
