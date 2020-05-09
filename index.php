d2760879ccfa960ec2af1683d71f67a71469729a
Kevin Silvestre <kevsilvo@gmail.com>
2020-05-09 08:58:36 -0400

    trying to display php on HTML

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include 'inc\functions.php'  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,8 +11,8 @@
 <body>
   <div class="container">
     <div id="quote-box">
-      <p class="quote">Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.</p>
-      <p class="source">Patrick McKenzie<span class="citation">Twitter</span><span class="year">2016</span></p>
+      <?php  printQuote($quote);  ?>
+      <?php  echo "Hello"  ?>
     </div>
     <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
   </div>
